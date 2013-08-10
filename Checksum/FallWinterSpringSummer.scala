8e6bf62fcfcf1e923a6a4ca05f01ca3e8d787f9b
Artem Shitov <inbox@artemshitov.ru>
2013-08-10 10:16:04 +0400

    Initial

---
+++ Checksum/FallWinterSpringSummer.scala
@@ -0,0 +1,7 @@
+import ChecksumAccumulator.calculate
+
+object FallWinterSpringSummer extends App {
+  for (season <- List("Fall", "Winter", "Spring")) {
+    println(season + ": " + calculate(season))
+  }
+}
