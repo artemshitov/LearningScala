5ccb60a9a4008e80b650b8e24abe3651a5800b41
Artem Shitov <inbox@artemshitov.ru>
2013-08-16 20:19:26 +0400

    Tests for Word

--- 08-PigLatin/src/main/scala/PigLatin.scala
+++ 08-PigLatin/src/main/scala/PigLatin.scala
@@ -1,20 +1,5 @@
 package ru.artemshitov.pig_latin
 
-// The usual rules for changing standard English into Pig Latin are as follows:
-// For words that begin with consonant sounds, the initial consonant or consonant cluster is moved to the end of the word, and "ay" is added, as in the following examples:
-// "happy" → "appyhay"
-// "duck" → "uckday"
-// "glove" → "oveglay"
-
-// For words that begin with vowel sounds or silent letter, "way" is added at the end of the word. Examples are
-// "egg" → "eggway"
-// "inbox" → "inboxway"
-// "eight" → "eightway"
-
-// The letter 'y' can play the role of either consonant or vowel, depending on its location
-// "yellow" → "ellowyay"
-// "rhythm" → "ythmrhay"
-
 class Word(original: String) {
   def toPigLatin: String = {
     if (original.startsWith("y"))
---
+++ 08-PigLatin/src/test/scala/WordTest.scala
@@ -0,0 +1,38 @@
+import org.scalatest.FunSuite
+import ru.artemshitov.pig_latin.Word
+
+class WordSuite extends FunSuite {
+  private def testWords(words: Map[String, String]) {
+    for ((key, value) <- words)
+      expectResult(value){new Word(key).toPigLatin}
+  }
+
+  test("words beginning with consonants") {
+    testWords(
+      Map(
+        "happy" -> "appyhay",
+        "duck" -> "uckday",
+        "glove" -> "oveglay"
+      )
+    )
+  }
+
+  test("words beginning with vowels") {
+    testWords(
+      Map(
+        "egg" -> "eggway",
+        "inbox" -> "inboxway",
+        "eight" -> "eightway"
+      )
+    )
+  }
+
+  test("words with y") {
+    testWords(
+      Map(
+        "yellow" -> "ellowyay",
+        "rhythm" -> "ythmrhay"
+      )
+    )
+  }
+}
