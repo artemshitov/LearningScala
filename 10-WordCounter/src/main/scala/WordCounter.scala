0e6d68791359a5723aba6b650333873b982da1eb
Artem Shitov <inbox@artemshitov.ru>
2013-08-18 21:31:20 +0400

    Simplify

--- 10-WordCounter/src/main/scala/WordCounter.scala
+++ 10-WordCounter/src/main/scala/WordCounter.scala
@@ -5,7 +5,7 @@ class WordCounter(s: String) {
   private[this] val counts =
     (Map[String, Int]() /: words) { (a, e) =>
       a + (e -> (a.getOrElse(e, 0) + 1))
-    }.toList.sortBy(_._2).reverse
+    }.toList.sortWith(_._2 > _._2)
 
   def top(size: Int) = counts.take(size)
   def uniqueWords = words.toSet
